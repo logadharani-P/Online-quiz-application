f4400adbb77101b501275fd6046a8f87f5a70a36
Logadharani P <misslogadharani@gmail.com>
2026-07-03 22:36:22 +0530

    Refactor app.py for database connection and routes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 # =========================
 def get_db():
         return mysql.connector.connect(
-        host="hayabusa.proxy.rlwy.net:50276/railway",
+        host="hayabusa.proxy.rlwy.net:50276",
         user="root",
         password="OjQjAuMykudnPWHfhhrSWnnNUvyTJYMu",
         database="railway",
